042a430fe36f64e4cbd57e243926e0482ba52be6
nikitatop304-stack <nikitatop304@gmail.com>
2026-01-11 20:46:44 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,15 @@
+BOT_TOKEN = "8263678337:AAFJcT8ZjVJig9S8I5WysICEhzkALUejm-s"
+ADMIN_IDS = [721329419]
+SUPPORT_USERNAME = "@eemux"
+
+# Новые цены подписок в USD
+SUBSCRIPTION_PRICES = {
+    "day": 0.1,      # 0.1$
+    "week": 1.0,     # 1$
+    "month": 3.0,    # 3$
+    "forever": 7.0   # 7$
+}
+
+# CryptoPay API
+CRYPTOPAY_API_TOKEN = "514387:AAwsNNelFVEWmNTCWgHxc4uaFEKC6fGEUUT"  # Получить на https://cryptopay.me
+CRYPTOPAY_API_URL = "https://pay.cryptopay.me/api/"
